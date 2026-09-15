6bd9e8992079d763b98f414af29f8adde7e3a221
Stephen Anti <stephenanti63@gmail.com>
2026-09-15 10:00:37 -0700

    1-registered routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import authRoute from "./routes/authRoute.js"
 
 const app = express();
 
@@ -24,6 +25,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({extended: true}));
 
 app.use(morgan("dev"));
+
+app.use("/api/user", authRoute);
 
 
 export default app;
