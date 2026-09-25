331cb3a7accdb3eaf7262e7f67d1cb58b3d5338a
Stephen Anti <stephenanti63@gmail.com>
2026-09-24 20:59:43 -0700

    1-proxy setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:6000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
